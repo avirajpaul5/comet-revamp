8ea186e8c07cc2fc92889ca453f33d30be93f86b
NachiketaTiwari <nachiketatiwari56@gmail.com>
2024-07-22 23:13:17 +0530

    reviews section added

--- src/Components/Reviews.js
+++ src/Components/Reviews.js
@@ -1,8 +1,60 @@
 import React from 'react'
+import "../Styles/Reviews.css"
 
 const Reviews = () => {
   return (
-    <div className="ReviewsContainer">Reviews</div>
+    <>
+      <div className='reviewSection'>
+        <h1>What're they Saying?</h1>
+        <div className="container">
+        <div className="gallery-wrap">
+          <div className="maal maal-1">
+            <div className="caption">
+              <h1>Love how this fits</h1>
+              <p>This card has a frosted glass effect using the backdrop-filter property. Try hovering over it to see the effect change.</p>
+            </div>
+          </div>
+          <div className="maal maal-2">
+            <div className="caption">
+              <h1></h1>
+              <p>This card has a frosted glass effect using the backdrop-filter property. Try hovering over it to see the effect change.</p>
+            </div>
+          </div>
+          <div className="maal maal-3">
+            <div className="caption">
+              <h1>Frosted Glass Effect</h1>
+              <p>This card has a frosted glass effect using the backdrop-filter property. Try hovering over it to see the effect change.</p>
+            </div>
+          </div>
+          <div className="maal maal-4">
+            <div className="caption">
+              <h1>Frosted Glass Effect</h1>
+              <p>This card has a frosted glass effect using the backdrop-filter property. Try hovering over it to see the effect change.</p>
+            </div>
+          </div>
+          <div className="maal maal-5">
+            <div className="caption">
+              <h1>Frosted Glass Effect</h1>
+              <p>This card has a frosted glass effect using the backdrop-filter property. Try hovering over it to see the effect change.</p>
+            </div>
+          </div>
+          <div className="maal maal-6">
+            <div className="caption">
+              <h1>Frosted Glass Effect</h1>
+              <p>This card has a frosted glass effect using the backdrop-filter property. Try hovering over it to see the effect change.</p>
+            </div>
+          </div>
+          <div className="maal maal-7">
+            <div className="caption">
+              <h1>Frosted Glass Effect</h1>
+              <p>This card has a frosted glass effect using the backdrop-filter property. Try hovering over it to see the effect change.</p>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+      </div>
+    </>
   )
 }
 
