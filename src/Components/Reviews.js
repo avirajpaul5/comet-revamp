06ac7c6cbe730ace202c32c191c5689fef44377e
adityaks-11 <adityakrsingh264@gmail.com>
2024-07-23 14:16:57 +0530

    developed the sliderShoes Section

--- src/Components/Reviews.js
+++ src/Components/Reviews.js
@@ -10,44 +10,44 @@ const Reviews = () => {
         <div className="gallery-wrap">
           <div className="maal maal-1">
             <div className="caption">
-              <h1>Love how this fits</h1>
-              <p>This card has a frosted glass effect using the backdrop-filter property. Try hovering over it to see the effect change.</p>
+              <h1>LOVE HOW THIS FITS</h1>
+              <p>I love how it fits and has a unique appeal, definitely not gonna feel shy or sorry while buying them again.</p>
             </div>
           </div>
           <div className="maal maal-2">
             <div className="caption">
-              <h1></h1>
-              <p>This card has a frosted glass effect using the backdrop-filter property. Try hovering over it to see the effect change.</p>
+              <h1>CRAZY STYLE AND COMFORT</h1>
+              <p>Great combination of style and comfort, Trumps a lot of the other brands I've won. Excited to keep adding Comets to my wardrobe.</p>
             </div>
           </div>
           <div className="maal maal-3">
             <div className="caption">
-              <h1>Frosted Glass Effect</h1>
-              <p>This card has a frosted glass effect using the backdrop-filter property. Try hovering over it to see the effect change.</p>
+              <h1>COLOURFUL, VIBRANT AND COMFY!</h1>
+              <p>I really liked the Comet X Lows in Lagoon. They're colourful, vibrant, and SO SO comfy - making them and absolutely perfect wear for cool events as well as for all the walking I like to do. Love them!</p>
             </div>
           </div>
           <div className="maal maal-4">
             <div className="caption">
-              <h1>Frosted Glass Effect</h1>
-              <p>This card has a frosted glass effect using the backdrop-filter property. Try hovering over it to see the effect change.</p>
+              <h1>THESE LOOK SO COOL</h1>
+              <p>I wanted a slide that i could wear outside to the malls and with my friends to cafes. Comet has that perfect blend of style and comfort.</p>
             </div>
           </div>
           <div className="maal maal-5">
             <div className="caption">
-              <h1>Frosted Glass Effect</h1>
-              <p>This card has a frosted glass effect using the backdrop-filter property. Try hovering over it to see the effect change.</p>
+              <h1>STATEMENT MAKER!</h1>
+              <p>Really a pair that stands out and how! Loved everything from the packaging of the Mango to the actual pair in hand. The coomfort is just amazing and the pair is a statement maker.</p>
             </div>
           </div>
           <div className="maal maal-6">
             <div className="caption">
-              <h1>Frosted Glass Effect</h1>
-              <p>This card has a frosted glass effect using the backdrop-filter property. Try hovering over it to see the effect change.</p>
+              <h1>I LOVE THESE SLIDES!</h1>
+              <p>I can pair these with all outfits and they look so great. I can't wait for wearing more colours in these. Launch soon.</p>
             </div>
           </div>
           <div className="maal maal-7">
             <div className="caption">
-              <h1>Frosted Glass Effect</h1>
-              <p>This card has a frosted glass effect using the backdrop-filter property. Try hovering over it to see the effect change.</p>
+              <h1>LOVE HOW IT FITS!</h1>
+              <p>Love the way they look and fit. I got my first pair of comets a few months back and loved them so much that i ended up buying another as well as gifted my wife a pair as well. They are trendy, comfy and highly recommended.</p>
             </div>
           </div>
           
@@ -57,5 +57,6 @@ const Reviews = () => {
     </>
   )
 }
+
 
 export default Reviews
--- src/Components/SliderShoes.js
+++ src/Components/SliderShoes.js
@@ -1,9 +1,83 @@
-import React from 'react'
+import React from 'react';
+import "../Styles/SliderShoes.css";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+import slider1 from "../Assets/sliderImages/slider 1.png"
+import slider2 from "../Assets/sliderImages/slider 2.png";
+import slider3 from "../Assets/sliderImages/slider 3.png";
 
-const SliderShoes = () => {
+const SliderShoesSection = () => {
+  useGSAP(() => {
+    
+    gsap.from(".bgText", {
+      opacity: 0,
+      // delay: 1,
+      scrub: 2,
+      duration: 7,
+      scrollTrigger: {
+        trigger: ".bgText",
+        scroller:"body",
+        // markers:true,
+        start:"top 30%",
+        end:"top -140%",
+      }
+    });
+    gsap.to(".bgText", {
+      y: -190,
+      yoyo: true,
+      scrollTrigger: {
+        trigger: ".bgText",
+        scroller:"body",
+        // markers:true,
+        start:"top 0%",
+        end:"top -140%",
+        delay: 1,
+        duration:7,
+        scrub:2,
+        // pin:true
+      }
+    });
+    gsap.to(".cardsContainer",{
+      transform:"translateY(-100%)",
+      scrollTrigger:{
+        trigger:".mainSliderContainer",
+        scroller:"body",
+        // markers:true,
+        start:"top 0%",
+        end:"top -110%",
+        scrub:2,
+        pin:true
+      }
+    });
+  });
+  
   return (
-    <div className="SliderShoesContainer">SliderShoes</div>
-  )
-}
+    <div className='mainSliderContainer'>
+      <div id="parallax-bg-3" className="cardsContainer">
+        <div id="bg-3-1" className="sliderCards">
+          <img src={slider1}></img>
+        </div>
+        <div id="bg-3-2" className="sliderCards">
+          <img src={slider2}></img>
+        </div>
+        <div id="bg-3-3" className="sliderCards">
+          <img src={slider3}></img>
+        </div>
+      </div>
+      <div id="parallax-bg-2" className="bgText">
+        <p id='small'>Cushy Foamy Goodness</p>
+        <p id='big'>SLIDE INTO SUMMER</p>
+      </div>
+    </div>
+  );
+};
 
-export default SliderShoes
+export default SliderShoesSection;
+
+
+
+
+
+
+
