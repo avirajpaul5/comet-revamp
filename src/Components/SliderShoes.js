8c87e6d30fbd58fce1ab97986e1e4730cbc229f1
adityaks-11 <adityakrsingh264@gmail.com>
2024-07-25 18:33:58 +0530

    updated sliderShoesSection

--- src/Components/SliderShoes.js
+++ src/Components/SliderShoes.js
@@ -7,12 +7,15 @@ import slider1 from "../Assets/sliderImages/slider1.png";
 import slider2 from "../Assets/sliderImages/slider2.png";
 import slider3 from "../Assets/sliderImages/slider3.png";
 import slider4 from "../Assets/sliderImages/slider4.png";
+import slider1hover from "../Assets/sliderImages/slider1hover.png";
+import slider2hover from "../Assets/sliderImages/slider2hover.png";
+import slider3hover from "../Assets/sliderImages/slider3hover.png";
+import slider4hover from "../Assets/sliderImages/slider4hover.png";
 
 const SliderShoesSection = () => {
   useGSAP(() => {
     gsap.from(".bgText", {
       opacity: 0,
-      // delay: 1,
       scrub: 2,
       duration: 7,
       scrollTrigger: {
@@ -34,7 +37,6 @@ const SliderShoesSection = () => {
         delay: 1,
         duration: 7,
         scrub: true,
-        // pin:true
       },
     });
     gsap.to(".cardsContainer", {
@@ -44,7 +46,7 @@ const SliderShoesSection = () => {
         scroller: "body",
         start: "top top",
         end: "top -180%",
-        scrub: 2,
+        scrub: 3,
         pin: true,
       },
     });
@@ -54,16 +56,20 @@ const SliderShoesSection = () => {
     <div className="mainSliderContainer">
       <div id="parallax-bg-3" className="cardsContainer">
         <div id="bg-3-1" className="sliderCards">
-          <img src={slider1}></img>
+          <img src={slider1} className="defaultImg" alt="Slider 1"></img>
+          <img src={slider1hover} className="imgOnHover" alt="Slider 1 Hover"></img>
         </div>
         <div id="bg-3-2" className="sliderCards">
-          <img src={slider2}></img>
+          <img src={slider2} className="defaultImg" alt="Slider 2"></img>
+          <img src={slider2hover} className="imgOnHover" alt="Slider 2 Hover"></img>
         </div>
         <div id="bg-3-3" className="sliderCards">
-          <img src={slider3}></img>
+          <img src={slider3} className="defaultImg" alt="Slider 3"></img>
+          <img src={slider3hover} className="imgOnHover" alt="Slider 3 Hover"></img>
         </div>
         <div id="bg-3-4" className="sliderCards">
-          <img src={slider4}></img>
+          <img src={slider4} className="defaultImg" alt="Slider 4"></img>
+          <img src={slider4hover} className="imgOnHover" alt="Slider 4 Hover"></img>
         </div>
       </div>
       <div id="parallax-bg-2" className="bgText">
